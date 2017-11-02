4cdaab8034b430129caaf9fde61fc31d2bdd4dfa
Tomoya AMACHI <tomoya.amachi@gmail.com>
2017-11-02 11:19:36 +0900

    add actions and components of post list

--- src/components/post_index.js
+++ src/components/post_index.js
@@ -1,13 +1,39 @@
+import _ from 'lodash';
 import React, {Component} from 'react';
+import {connect} from 'react-redux';
+import {fetchPosts} from '../actions';
 
 class PostsIndex extends Component{
+    // react lifecycle : react automate loaded
+    componentDidMount() {
+        this.props.fetchPosts();
+    }
+    renderPosts() {
+        return _.map(this.props.posts,
+                     post => {
+                         return (
+                                 <li className="list-group-item" key={post.id}>
+                                 {post.title }
+                                 </li>
+                         );
+                     }
+                    );
+    }
     render() {
         return (
             <div>
-                PostIndex
+                <h3>PostsIndex</h3>
+                <ul className="list-group">
+                {this.renderPosts()}
+                </ul>
             </div>
         );
     }
 }
 
-export default PostsIndex;
+function mapStateToProps(state) {
+    return {posts: state.posts}
+}
+
+// connect (state, propertyName)
+export default connect(mapStateToProps, {fetchPosts})(PostsIndex);
