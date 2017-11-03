35f55e030df23f39e786407ed703a129ae4527fd
Tomoya AMACHI <tomoya.amachi@gmail.com>
2017-11-03 12:15:22 +0900

    lesson 144: create post show component

---
+++ src/components/posts_show.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class PostsShow extends Component {
+    render() {
+        return (
+            <div>
+              posts new
+            </div>
+        );
+    }
+}
+
+export default PostsShow;
--- src/index.js
+++ src/index.js
@@ -10,6 +10,7 @@ import reducers from './reducers';
 import promise from 'redux-promise';
 import PostsIndex from './components/posts_index';
 import PostsNew from './components/posts_new';
+import PostsShow from './components/posts_show';
 
 const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
 
@@ -19,6 +20,7 @@ ReactDOM.render(
       <div>
         <Switch>
           <Route path="/posts/new" component={PostsNew} />
+          <Route path="/posts/:id" component={PostsShow} />
           <Route path="/" component={PostsIndex} />
         </Switch>
       </div>
