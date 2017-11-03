c607df790f4d0c6473ab79b2f21858749e3b4deb
Tomoya AMACHI <tomoya.amachi@gmail.com>
2017-11-03 13:18:12 +0900

    lesson 151: to memory leak

--- src/reducers/reducer_posts.js
+++ src/reducers/reducer_posts.js
@@ -1,8 +1,13 @@
 import _ from 'lodash';
-import {FETCH_POSTS, FETCH_POST} from '../actions';
+import {FETCH_POSTS, FETCH_POST, DELETE_POST} from '../actions';
 
 export default function(state = {}, action) {
     switch (action.type) {
+    case DELETE_POST:
+        // Delete post data from state
+        // payload = post.id
+        return _.omit(state, action.payload);
+        // same as : return _.refect(state, post => post.id === action.payload);
     case FETCH_POSTS:
         return _.mapKeys(action.payload.data, 'id');
     case FETCH_POST:
