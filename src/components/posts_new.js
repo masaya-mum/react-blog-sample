4751ae35eff5f7053c5c5e2e57907b0e564edc57
Tomoya AMACHI <tomoya.amachi@gmail.com>
2017-11-03 10:53:27 +0900

    lecture 136 done : create form validation

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -27,8 +27,8 @@ class PostsNew extends Component {
                 component={this.renderField}
                 />
               <Field
-                label="Tags"
-                name="tags"
+                label="Categories"
+                name="categories"
                 component={this.renderField}
                 />
               <Field
@@ -36,11 +36,34 @@ class PostsNew extends Component {
                 name="content"
                 component={this.renderField}
                 />
-
             </form>
         );
     }
 }
+
+function validate(values) {
+    // values : -> { title: 'a', catecories: 'test', content: 'hoge' }
+    const errors = {};
+
+    // Validate the ipunts from 'values'
+    if(!values.title || values.title.length < 3) {
+        errors.title = "enter a title that is at least 3 chars!"
+    }
+
+    if(!values.categories) {
+        errors.categories = "enter a categories!"
+    }
+
+    if(!values.content) {
+        errors.content = "enter a content!"
+    }
+
+    // if errors is empty, the form is fine to submit
+    // if errors has any properties, redux-form assumes form is invalid
+    return errors;
+}
+
 export default reduxForm({
+    validate,
     form: 'PostsNewForm'
 })(PostsNew);
